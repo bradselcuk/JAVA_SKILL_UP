bd8c4703e460108a0d2316ef76a619e9565f7c41
bradselcuk <84919587+bradselcuk@users.noreply.github.com>
2021-07-20 22:50:07 -0600

    Update Intro_1.java

--- src/App/Intro/Intro_1.java
+++ src/App/Intro/Intro_1.java
@@ -3,7 +3,7 @@ package App.Intro;
 public class Intro_1 {
     public static void main(String[] args) {
 
-        int a = 10;
+        int a = 38;
         int b = 20;
         System.out.println(a+b);
     }
