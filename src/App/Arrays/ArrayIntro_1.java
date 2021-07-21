a97d5177b8b709b165c56afcfcee8e929292e0c6
bradselcuk <84919587+bradselcuk@users.noreply.github.com>
2021-07-20 22:44:16 -0600

    Update ArrayIntro_1.java

--- src/App/Arrays/ArrayIntro_1.java
+++ src/App/Arrays/ArrayIntro_1.java
@@ -4,7 +4,7 @@ public class ArrayIntro_1 {
     public static void main(String[] args) {
 
         int [] myNum = {12,30,40,21,89};
-        System.out.println(myNum[0]);         // array index
+        System.out.println(myNum[1]);         // array index
 
         for (int i =0; i< myNum.length; i++){ // for loop
             System.out.println(myNum[i]);
